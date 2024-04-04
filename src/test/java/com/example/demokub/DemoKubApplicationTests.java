f7c19e262293a1a47212533b9fe8d25bec6db168
Grigor99 <80825619+Grigor99@users.noreply.github.com>
2024-04-04 13:09:44 +0400

    first commit of github actions

---
+++ src/test/java/com/example/demokub/DemoKubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demokub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoKubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
